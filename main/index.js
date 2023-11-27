bec21482f1b8408032edc14a31feec3986e68ee1
RihoMiyamoto <rmiyamoto7890@gmail.com>
2023-11-27 11:10:59 -0600

    new format

---
+++ main/index.js
@@ -0,0 +1,26 @@
+let currentSliderPosition = 0;
+
+function moveSliderRight() {
+    const sliderContainer = document.querySelector(".slider-container");
+    const done = document.querySelector(".testing")
+    done.style.color = "red";
+    if(currentSliderPosition != -200)
+    {
+        currentSliderPosition-=100;
+        sliderContainer.style.transform = `translateX(${currentSliderPosition}vw)`;
+    }
+}
+
+function moveSliderLeft() {
+    const sliderContainer = document.querySelector(".slider-container");
+    if(currentSliderPosition != 0)
+    {
+        currentSliderPosition+=100;
+        sliderContainer.style.transform = `translateX(${currentSliderPosition}vw)`;
+    }
+}
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
