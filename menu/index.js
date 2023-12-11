ff2212ca1463aceeb09375186b07a29c4cdc5089
RihoMiyamoto <rmiyamoto7890@gmail.com>
2023-12-11 13:54:36 -0600

    fix bugs

--- menu/index.js
+++ menu/index.js
@@ -1,10 +1,4 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-<<<<<<< Updated upstream
 })
-=======
-})
-//https://stackoverflow.com/questions/5902822/stopping-fixed-position-scrolling-at-a-certain-point
-
->>>>>>> Stashed changes
