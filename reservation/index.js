b10a4e55dc491be986ca9c7cc4dba75fdd41d30c
RihoMiyamoto <rmiyamoto7890@gmail.com>
2023-12-11 13:40:10 -0600

    reservation update

--- reservation/index.js
+++ reservation/index.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 })
+
+document.getElementById("submitBtn").addEventListener("click", showInput);  
+function showInput() {  
+  window.location.href="../menu/index.html";  
+}
