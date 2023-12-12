4a604e8be054731b9f2658cbce224d196e5a2077
RihoMiyamoto <rmiyamoto7890@gmail.com>
2023-12-12 16:48:42 -0600

    final edit

--- reservation/index.js
+++ reservation/index.js
@@ -6,24 +6,28 @@ window.addEventListener("scroll", function () {
 
 function showInput() {
   var firstName = document.getElementById('Name').value;
-  var lastName = document.getElementById('last');
-  var email = document.getElementById('email');
-  var date = document.getElementById('date');
-  var seat = document.getElementsByClassName('seat');
-  var table = document.getElementById('table-num');
-  var message = document.getElementById('message');
+  var lastName = document.getElementById('last').value;
+  var email = document.getElementById('email').value;
+  var date = document.getElementById('date').value;
+  var time = document.getElementById('time').value;
+  var seat = document.getElementsByName('seat').value;
 
+  var radioButtonGroup = document.getElementsByName("seat");
+  var checkedRadio = Array.from(radioButtonGroup).find(
+    (radio) => radio.checked
+  );
+  var table = document.getElementById('table-num').value;
+  var message = document.getElementById('message').value;
 
-  console.log(firstName);
   document.getElementById("sName").innerHTML = firstName;
   document.getElementById("sLastName").innerHTML = lastName;
   document.getElementById("sEmail").innerHTML = email;
   document.getElementById("sDate").innerHTML = date;
-  document.getElementById("sSeat").innerHTML = seat;
+  document.getElementById("sTime").innerHTML = time;
   document.getElementById("sTableNum").innerHTML = table;
   document.getElementById("sMessage").innerHTML = message;
-  document.getElementById('registration-form').style.display ='hidden';   
   document.getElementById('hidden').style.display = 'flex'; // Show the div
+  document.getElementById('registration-form').style.display = 'none';
 }
 
 document.addEventListener('DOMContentLoaded', function () {
