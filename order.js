317c02f962c910c4dd91f5f30d4f3ca4c62d48ad
RihoMiyamoto <rmiyamoto7890@gmail.com>
2023-12-12 13:16:13 -0600

    cart check

--- order.js
+++ order.js
@@ -11,6 +11,31 @@ let counter = 0;
 let currentSliderPosition = 0;
 
 
+function moveSliderRight() {
+
+    const sliderContainer = document.querySelector(".slider-container");
+    if(currentSliderPosition != -200)
+    {
+        currentSliderPosition-=100;
+        sliderContainer.style.transform = `translateX(${currentSliderPosition}vw)`;
+    }
+}
+
+function moveSliderLeft() {
+    const sliderContainer = document.querySelector(".slider-container");
+    if(currentSliderPosition != 0)
+    {
+        currentSliderPosition+=100;
+        sliderContainer.style.transform = `translateX(${currentSliderPosition}vw)`;
+    }
+}
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
